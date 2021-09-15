8720bce9f2815d259626ff5c3c57af7e4ff74244
letsgetcodin <90793445+letsgetcodin@users.noreply.github.com>
2021-09-15 17:55:12 -0400

    Add files via upload

---
+++ spaceship.py
@@ -0,0 +1,21 @@
+#########################
+# Spaceship Starter Code
+#
+# Author: Me
+#########################
+
+print("Welcome traveler!")
+
+name = input("What is your name? ")
+
+print("Hello {}!".format(name))
+
+companions = int(input("How many companions do you have? "))
+
+# Add your input code here
+
+planet = input("Where do you want to go? ")
+
+# Add your code here
+
+print("The Spaceship is taking off with {} people to planet {} ".format(companions+1, planet))
